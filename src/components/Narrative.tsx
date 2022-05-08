c836adfe0c2bd41ae4cd77732061e170aa3b8839
Lester Lee <contact@lester-lee.com>
2022-05-08 10:12:43 -0400

    Condense narrative components

--- src/components/Narrative.tsx
+++ src/components/Narrative.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import styled from "@emotion/styled";
 
 import { NARRATIVE } from "../constants/copy";
-import Title from "./Title";
-import Introduction from "./Introduction";
 import Page from "./Page";
 
 const Article = styled("article")`
@@ -15,6 +13,26 @@ const Article = styled("article")`
   overflow-y: scroll;
 `;
 
+const TITLE = (
+  <Page id="title" title={"An Exploration of NYC School Budgets"} />
+);
+
+const INTRODUCTION = (
+  <>
+    <Page
+      title="Introduction"
+      body={`I grew up in NYC public schools, from pre-K through high school, and it was something I had always taken for granted. It was not until I became a high school teacher myself that I began to question the intricate—and obfuscated—inner workings of public education.`}
+    />
+    <Page
+      title="Introduction"
+      body={`How much money is actually spent each year? How is it being spent? Is there not enough money, or is it being poorly managed? I did not have concrete numbers for any of these questions, so I did some digging to see what I could find and learn.`}
+    />
+    <Page
+      title="Introduction"
+      body={`I'm not an expert, and this project is mainly an educational tool for myself. I want to emphasize that this is nowhere near comprehensive, but I hope that these numbers provide a good starting place for anyone looking to learn more.`}
+    />
+  </>
+);
 
 type Props = {
   onScroll: (event: React.UIEvent<HTMLElement>) => void;
@@ -23,9 +41,8 @@ type Props = {
 const Narrative = ({ onScroll }: Props) => {
   return (
     <Article onScroll={onScroll}>
-      <Title/>
-      <Introduction />
-
+      {TITLE}
+      {INTRODUCTION}
       {NARRATIVE.map((page, i) => (
         <Page key={i} title={page.title} body={page.body} />
       ))}
